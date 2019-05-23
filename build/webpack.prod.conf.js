ee692abe9ae94c8722085ebdb66006a9a18b31dc
TonyLip <lin_jinhuan006@163.com>
2019-05-23 16:55:38 +0800

    配置favicon

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -67,6 +67,7 @@ const webpackConfig = merge(baseWebpackConfig, {
       filename: config.build.index,
       template: 'index.html',
       inject: true,
+      favicon: path.resolve(__dirname, '../static/logo.ico'),
       minify: {
         removeComments: true,
         collapseWhitespace: true,
