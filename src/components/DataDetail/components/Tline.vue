dec7399a6dee54526e37de2cb1e959a19630ec02
TonyLip <lin_jinhuan006@163.com>
2019-08-15 20:31:33 +0800

    修改数据视图中线缆显示方式

--- src/components/DataDetail/components/Tline.vue
+++ src/components/DataDetail/components/Tline.vue
@@ -1,5 +1,50 @@
 <template>
     <div>
+        <div>
+            <el-row :gutter="20">
+                <el-col :span="12" :xs="24">
+                    <div class="info">
+                        <el-popover
+                            placement="bottom"
+                            width="200"
+                            trigger="click"
+                        >
+                            <div class="pop">
+                                <span v-for="i in 5">
+                                    线缆电压 : <span>0</span>A
+                                </span>
+                            </div>
+                            <el-link type="primary" slot="reference">线缆一</el-link>
+                        </el-popover>
+                        <strong class="msg">
+                            线缆温度:<span>10</span>℃ &nbsp;&nbsp;震动:<span>静止</span>
+                        </strong>
+                        <el-progress :percentage="50" color="#f56c6c" :show-text="false"></el-progress>
+                    </div>
+                </el-col>
+                <el-col :span="12"  :xs="24">
+                    <div class="info">
+                        <el-popover
+                            placement="bottom"
+                            width="200"
+                            trigger="click"
+                        >
+                            <div class="pop">
+                                <span v-for="i in 5">
+                                    线缆电压 : <span>0</span>A
+                                </span>
+                            </div>
+                            <el-link type="primary" slot="reference">线缆一</el-link>
+                        </el-popover>
+                        <strong class="msg">
+                            线缆温度:<span>10</span>℃ &nbsp;&nbsp;震动:<span>静止</span>
+                        </strong>
+                        <el-progress :percentage="50" color="#f56c6c" :show-text="false"></el-progress>
+                    </div>
+                </el-col>
+            </el-row>
+        </div>
+        <el-divider content-position="left">线缆历史数据</el-divider>
         <div class="seletGroup">
             <el-form label-position="top">
                 <el-form-item label="环境变量:">
@@ -59,5 +104,17 @@
 </script>
 
 <style lang="scss" scoped>
-
+    .info{
+        padding-bottom: 20px;
+        position: relative;
+        .msg{
+            position: absolute;
+            right: 0;
+            top: 5px;
+            font-size: 14px;
+            span{
+                color: #f56c6c;
+            }
+        }
+    }
 </style>
