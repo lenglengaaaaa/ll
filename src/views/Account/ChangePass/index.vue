c908d495fd207b607bbbc45480484b7c4a6d2763
TonyLip <lin_jinhuan006@163.com>
2019-09-05 17:34:40 +0800

    调整修改页面布局

--- src/views/Account/ChangePass/index.vue
+++ src/views/Account/ChangePass/index.vue
@@ -1,48 +1,56 @@
 <template>
     <div class="change_pass">
-        <el-form 
-            label-position="top" 
-            :model="ruleForm" 
-            status-icon 
-            :rules="rules" 
-            ref="ruleForm" 
-        >
-            <h4>修改用户密码</h4>
-            <el-form-item label="原密码" prop="oldPassword">
-                <el-input  type="password" v-model="ruleForm.oldPassword"></el-input>
-            </el-form-item>
-            <el-form-item label="密码" prop="firstNewPassword">
-                <el-input type="password" v-model="ruleForm.firstNewPassword" autocomplete="off"></el-input>
-            </el-form-item>
-            <el-form-item label="确认密码" prop="againNewPassword">
-                <el-input type="password" v-model="ruleForm.againNewPassword" autocomplete="off"></el-input>
-            </el-form-item>
-            <el-form-item class="submit">
-                <el-button type="primary" @click="submitForm('ruleForm')">提交</el-button>
-                <el-button @click="resetForm('ruleForm')">重置</el-button>
-            </el-form-item>
-        </el-form>
-        <el-form 
-            label-position="top" 
-            :model="ruleForm" 
-            status-icon :rules="rules" 
-            ref="operateForm" 
-        >
-            <h4>修改操作密码</h4>
-            <el-form-item label="用户密码" prop="pass">
-                <el-input type="password" v-model="ruleForm.pass" autocomplete="off"></el-input>
-            </el-form-item>
-            <el-form-item label="操作密码" prop="operationPwd">
-                <el-input type="password" v-model="ruleForm.operationPwd" autocomplete="off" maxlength="6"></el-input>
-            </el-form-item>
-            <el-form-item label="确认操作密码" prop="checkOperationPwd">
-                <el-input type="password" v-model="ruleForm.checkOperationPwd" autocomplete="off" maxlength="6"></el-input>
-            </el-form-item>
-            <el-form-item class="submit">
-                <el-button type="primary" @click="submitForm('operateForm')">提交</el-button>
-                <el-button @click="resetForm('operateForm')">重置</el-button>
-            </el-form-item>
-        </el-form>
+        <el-card class="box-card">
+            <div slot="header" class="clearfix">
+                <strong>修改用户密码</strong>
+            </div>
+            <el-form 
+                label-position="top" 
+                :model="ruleForm" 
+                status-icon 
+                :rules="rules" 
+                ref="ruleForm" 
+            >
+                <el-form-item label="原密码" prop="oldPassword">
+                    <el-input  type="password" v-model="ruleForm.oldPassword"></el-input>
+                </el-form-item>
+                <el-form-item label="密码" prop="firstNewPassword">
+                    <el-input type="password" v-model="ruleForm.firstNewPassword" autocomplete="off"></el-input>
+                </el-form-item>
+                <el-form-item label="确认密码" prop="againNewPassword">
+                    <el-input type="password" v-model="ruleForm.againNewPassword" autocomplete="off"></el-input>
+                </el-form-item>
+                <el-form-item class="submit">
+                    <el-button type="primary" @click="submitForm('ruleForm')">提交</el-button>
+                    <el-button @click="resetForm('ruleForm')">重置</el-button>
+                </el-form-item>
+            </el-form>
+        </el-card>
+        <el-card class="box-card">
+            <div slot="header" class="clearfix">
+                <strong>修改操作密码</strong>
+            </div>
+            <el-form 
+                label-position="top" 
+                :model="ruleForm" 
+                status-icon :rules="rules" 
+                ref="operateForm" 
+            >
+                <el-form-item label="用户密码" prop="pass">
+                    <el-input type="password" v-model="ruleForm.pass" autocomplete="off"></el-input>
+                </el-form-item>
+                <el-form-item label="操作密码" prop="operationPwd">
+                    <el-input type="password" v-model="ruleForm.operationPwd" autocomplete="off" maxlength="6"></el-input>
+                </el-form-item>
+                <el-form-item label="确认操作密码" prop="checkOperationPwd">
+                    <el-input type="password" v-model="ruleForm.checkOperationPwd" autocomplete="off" maxlength="6"></el-input>
+                </el-form-item>
+                <el-form-item class="submit">
+                    <el-button type="primary" @click="submitForm('operateForm')">提交</el-button>
+                    <el-button @click="resetForm('operateForm')">重置</el-button>
+                </el-form-item>
+            </el-form>
+        </el-card>
     </div>
 </template>
 
@@ -178,24 +186,21 @@
         display: flex;
         align-items: center;
         justify-content: center;
-        .el-form{
-            width:500px;
-            max-width: 100%;
-            padding: 15px 10px;
-            background:#fff;
-            box-shadow: 0 1px 1px hsla(204,8%,76%,.8);
-            margin: 20px;
-            h4{
-                margin: 5px 0 15px 0 ;
-            }
-            .el-input__inner{
-                border-radius: 0px;
-                height: 35px;
-                line-height: 35px;
-            }
-            .submit{
-                padding-top: 20px;
-                text-align: center;
+        .box-card{
+            margin: 0 10px 10px 0;
+            .el-form{
+                width:500px;
+                max-width: 100%;
+                background:#fff;
+                .el-input__inner{
+                    border-radius: 0px;
+                    height: 35px;
+                    line-height: 35px;
+                }
+                .submit{
+                    padding-top: 20px;
+                    text-align: center;
+                }
             }
         }
     }
