223cf7fedcd45c883a0522c8d53f5911dda442e5
Bazinga <lin_jinhuan006@163.com>
2019-12-30 15:56:57 +0800

    优化webpack配置 , 优化打包速度 & 优化打包文件体积

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -4,11 +4,21 @@ const utils = require('./utils')
 const config = require('../config')
 const vueLoaderConfig = require('./vue-loader.conf')
 
+const webpack = require('webpack');
+const CopyWebpackPlugin=require('copy-webpack-plugin');
+
+const HappyPack = require('happypack');
+const os = require('os');
+const happyThreadPool = HappyPack.ThreadPool({ size: os.cpus().length });
+
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+// 引入 ParallelUglifyPlugin 插件
+const ParallelUglifyPlugin = require('webpack-parallel-uglify-plugin');
+
 function resolve (dir) {
   return path.join(__dirname, '..', dir)
 }
-
-
 
 module.exports = {
   context: path.resolve(__dirname, '../'),
@@ -25,13 +35,17 @@ module.exports = {
   externals: {
     '$':'jquery',
     'moment': 'moment',
-    'vue': 'Vue',
+    // 'vue': 'Vue',
     'vue-router': 'VueRouter',
     'vuex': 'Vuex',
     'echarts': 'echarts',
   },
   resolve: {
     extensions: ['.js', '.vue', '.json'],
+    modules: [
+      resolve('src'),
+      resolve('node_modules')
+    ],
     alias: {
       'vue$': 'vue/dist/vue.esm.js',
       '@': resolve('src'),
@@ -43,12 +57,15 @@ module.exports = {
     rules: [
       {
         test: /\.vue$/,
-        loader: 'vue-loader',
-        options: vueLoaderConfig
+        loader: 'happypack/loader?id=vue'
+        // loader: 'vue-loader',
+        // options: vueLoaderConfig
       },
       {
         test: /\.js$/,
-        loader: 'babel-loader',
+        //把对.js 的文件处理交给id为happyBabel 的HappyPack 的实例执行
+        loader: 'happypack/loader?id=happyBabel',
+        // loader: 'babel-loader',
         include: [resolve('src'), resolve('test'), resolve('node_modules/webpack-dev-server/client')]
       },
       {
@@ -89,6 +106,57 @@ module.exports = {
       }
     ]
   },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HappyPack({
+      id: 'vue',
+      loaders: [{
+        loader: 'vue-loader',
+        options: vueLoaderConfig
+      }],
+    }),
+    new HappyPack({
+      //用id来标识 happypack处理那里类文件
+      id: 'happyBabel',
+      //如何处理  用法和loader 的配置一样
+      loaders: [{
+        loader: 'babel-loader?cacheDirectory=true',
+      }],
+      //共享进程池
+      threadPool: happyThreadPool,
+      //允许 HappyPack 输出日志
+      verbose: true,
+    }),
+    new webpack.DllReferencePlugin({
+      context: __dirname,
+      manifest: require('./vendor-manifest.json')
+    }),
+    new CopyWebpackPlugin([ // 拷贝生成的文件到dist目录 这样每次不必手动去cv
+      {from: 'static', to:'static'}
+    ]),
+    // 使用 ParallelUglifyPlugin 并行压缩输出JS代码
+    new ParallelUglifyPlugin({
+      // 传递给 UglifyJS的参数如下：
+      uglifyJS: {
+        output: {
+          /* 是否输出可读性较强的代码，即会保留空格和制表符，默认为输出，为了达到更好的压缩效果，可以设置为false */
+          beautify: false,
+          /* 是否保留代码中的注释，默认为保留，为了达到更好的压缩效果，可以设置为false */
+          comments: false
+        },
+        compress: {
+          /* 是否删除代码中所有的console语句，默认为不删除，开启后，会删除所有的console语句 */
+          drop_console: false,
+          /* 是否内嵌虽然已经定义了，但是只用到一次的变量，比如将 var x = 1; y = x, 转换成 y = 5, 默认为不转换，为了达到更好的压缩效果，可以设置为false */
+          collapse_vars: false,
+          /* 是否提取出现了多次但是没有定义成变量去引用的静态值，比如将 x = 'xxx'; y = 'xxx'  转换成
+            var a = 'xxxx'; x = a; y = a; 默认为不转换，为了达到更好的压缩效果，可以设置为false
+          */
+          reduce_vars: false
+        }
+      }
+    }),
+  ],
   node: {
     // prevent webpack from injecting useless setImmediate polyfill because Vue
     // source contains it (although only uses it if it's native).
